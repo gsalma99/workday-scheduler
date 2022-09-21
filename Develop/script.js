b22ab23fadca10dda2e7ee70fba534c49104b6dc
gsalma99 <salmagonzalezzz10011@gmail.com>
2022-09-20 20:31:47 -0500

    update on js

--- Develop/script.js
+++ Develop/script.js
@@ -1,2 +1,24 @@
+const timeBlocks = document.querySelectorAll("textarea")
+
+
 var time = moment().format("MMM Do, YYYY, hh:mm:ss");
 $("#currentDay").text(time);
+
+function assessTimeBlocks() {
+    let startTime = 9
+const currentHour = moment().hour();
+
+
+timeBlocks.forEach(timeBlock => {
+    if(currentHour === startTime) {timeBlock.classList.add('present')}
+    if(currentHour < startTime) {timeBlock.classList.add('future')}
+    if(currentHour > startTime) {timeBlock.classList.add('past')}
+    startTime ++
+})
+}
+function saveNote() {
+
+}
+$(".saveBtn").click(saveNote);
+
+assessTimeBlocks()
