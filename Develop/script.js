2de848c3026e1892334a802aa310f813c626e039
gsalma99 <salmagonzalezzz10011@gmail.com>
2022-09-21 23:59:24 -0500

    updated js and readme

--- Develop/script.js
+++ Develop/script.js
@@ -26,6 +26,11 @@ function saveNote() {
   var timeBlockId = siblings[1].getAttribute("id");
   localStorage.setItem(timeBlockId, text);
 }
+for (let i = 0; i < localStorage.length; i++){
+    let key = localStorage.key(i);
+    let value = localStorage.getItem(key);
+    console.log(key, value);
+  }
 $(".saveBtn").click(saveNote);
 
 assessTimeBlocks();
