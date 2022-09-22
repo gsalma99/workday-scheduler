7311112b3da3febc2f0029a6ccada665efc96cf7
gsalma99 <salmagonzalezzz10011@gmail.com>
2022-09-21 22:52:43 -0500

    updated my local storage and html

--- Develop/script.js
+++ Develop/script.js
@@ -1,24 +1,31 @@
-const timeBlocks = document.querySelectorAll("textarea")
-
+const timeBlocks = document.querySelectorAll("textarea");
 
 var time = moment().format("MMM Do, YYYY, hh:mm:ss");
 $("#currentDay").text(time);
-
 function assessTimeBlocks() {
-    let startTime = 9
-const currentHour = moment().hour();
+  let startTime = 9;
 
+  const currentHour = moment().hour();
 
-timeBlocks.forEach(timeBlock => {
-    if(currentHour === startTime) {timeBlock.classList.add('present')}
-    if(currentHour < startTime) {timeBlock.classList.add('future')}
-    if(currentHour > startTime) {timeBlock.classList.add('past')}
-    startTime ++
-})
+  timeBlocks.forEach((timeBlock) => {
+    if (currentHour === startTime) {
+      timeBlock.classList.add("present");
+    }
+    if (currentHour < startTime) {
+      timeBlock.classList.add("future");
+    }
+    if (currentHour > startTime) {
+      timeBlock.classList.add("past");
+    }
+    startTime++;
+  });
 }
 function saveNote() {
-
+  var siblings = $(this).siblings();
+  var text = siblings[1].value;
+  var timeBlockId = siblings[1].getAttribute("id");
+  localStorage.setItem(timeBlockId, text);
 }
 $(".saveBtn").click(saveNote);
 
-assessTimeBlocks()
+assessTimeBlocks();
